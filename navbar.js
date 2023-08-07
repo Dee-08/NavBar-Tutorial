63e0f00f4787355435e08478341dc4b16bb38573
Daniel David <danielibukun2000@gmail.com>
2023-08-07 22:11:40 +0100

    new commit

---
+++ navbar.js
@@ -0,0 +1,9 @@
+// let hom = document.querySelector('.home');
+
+// function colorChange() {
+//     document.body.style.background = rgba(122, 229, 243, 0.986);
+// }
+
+// home.addEventListener('mouseover', () => { 
+//     hom.innerHTML = 'HOME';
+// })
